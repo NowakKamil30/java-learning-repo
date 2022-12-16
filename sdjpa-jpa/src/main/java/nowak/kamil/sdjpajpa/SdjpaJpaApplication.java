14d00d80d8a037fb1b9a325bc3473ddd44a64269
Kamil Nowak <kamil@kamils-air.home>
2022-12-16 18:28:34 +0100

    init project

---
+++ sdjpa-jpa/src/main/java/nowak/kamil/sdjpajpa/SdjpaJpaApplication.java
@@ -0,0 +1,13 @@
+package nowak.kamil.sdjpajpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SdjpaJpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SdjpaJpaApplication.class, args);
+    }
+
+}
